1443e8f337b6f957a47c344e686499f4509c4d68
Sarthak <sarthaksharma070@gmail.com>
2020-09-03 00:51:52 +0530

    Cleaned Mileage and Engine Rows

--- pred.py
+++ pred.py
@@ -17,7 +17,60 @@ import matplotlib.pyplot as plt
 #Importing Data 
 df = pd.read_excel("Data_Train.xlsx")
 
+
 df.head()
 df.shape
 df.info()
+
+
+
+'''mileage = df[df['Mileage']].apply ( lambda x: x.split(' ')[0])'''
+
+
+""" MILEAGE """
+# removing the units from mileage 
+def setmileage(daf):
+    d = str(daf.Mileage).split(" ")[0]
+    return float(d)
+
+df["Mileage"] = df.apply(setmileage, axis=1)
+
+#Filling null values 
+df['Mileage'].fillna(df['Mileage'].median(), inplace=True)
+
+#Remvoing all 0 fig outliers
+df = df[df.Mileage != 0]
+
+
+
+
+"""   ENGINE   """
+# removing the units from engine
+def setengine(daf):
+    d = str(daf.Engine).split(" ")[0]
+    return float(d)
+
+df["Engine"] = df.apply(setengine, axis=1)
+
+#Filling null values 
+df['Engine'].fillna(df['Engine'].mean(), inplace=True)
+
+
+"""------------------ ENDED HERE-------------------"""
+
+
+"""    POWER     """
+#Removing 
+def setpower(daf):
+    d = str(daf.Power).split(" ")[0]
+    if d == "null":
+        return 0.00
+    else:
+        return float(d)
+
+df["Power"] = df.apply(setpower, axis=1)
+
+
+
+
 
