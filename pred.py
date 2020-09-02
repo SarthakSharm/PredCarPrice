0b533c945e28436b8a54f4bb6b9d40ebfce72831
Sarthak <sarthaksharma070@gmail.com>
2020-09-02 23:50:02 +0530

    Imported necessary headers and files

--- pred.py
+++ pred.py
@@ -4,3 +4,20 @@ Created on Wed Sep  2 23:10:17 2020
 
 @author: sarth
 """
+
+#Importing necessary Header files 
+
+import pandas as pd
+import numpy as np
+import math
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+#Importing Data 
+df = pd.read_excel("Data_Train.xlsx")
+
+df.head()
+df.shape
+df.info()
+
